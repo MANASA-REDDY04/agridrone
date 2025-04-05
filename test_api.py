e5d774b2ae7f1bd887c41b0e4c1ea269ce995068
Manasa <kandadimanasareddy12@gmail.com>
2025-04-05 16:44:37 +0530

    base url

--- test_api.py
+++ test_api.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-BASE_URL = "http://localhost:8000"
+BASE_URL = "https://agridrone.onrender.com/"
 
 def test_root_endpoint():
     """Test the root endpoint of the API"""
